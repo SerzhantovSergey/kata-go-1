1c580250fdc729bf15e011513871fcdd9d45c638
serzhantov <sergey.serzhantov264@gmail.com>
2023-02-11 15:38:51 +0700

    makenew homework make v3

--- module2/types/composite/makenew/homework/make/main.go
+++ module2/types/composite/makenew/homework/make/main.go
@@ -10,8 +10,9 @@ func main() {
 	products := getProducts()      // достаем продукты пользователей
 	m := make(map[int64][]Product) // создаем мапу, чтобы не итерировать цикл в цикле
 	var i int
+
 	for i = range products {
-		m[products[i].UserID] = append(m[products[i].UserID], products[i]) // записываем продукты в map по UserID
+		m[products[i].UserID] = append(m[products[i].UserID], products[i])
 	}
 
 	for i = range users { // итерируем пользователей один раз

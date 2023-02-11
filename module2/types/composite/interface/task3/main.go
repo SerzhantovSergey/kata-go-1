2cebed56b4deee7ad996393a17b2b0438341025d
serzhantov <sergey.serzhantov264@gmail.com>
2023-02-11 13:49:51 +0700

    interface homework task3

---
+++ module2/types/composite/interface/task3/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	ID   int
+	Name string
+}
+
+func (u *User) GetName() string {
+	return u.Name
+}
+
+type Userer interface {
+	GetName() string
+}
+
+func main() {
+	//var v []Userer
+
+	u := []User{
+		{
+			ID:   34,
+			Name: "Annet",
+		},
+		{
+			ID:   55,
+			Name: "John",
+		},
+		{
+			ID:   89,
+			Name: "Alex",
+		},
+	}
+	v := make([]Userer, len(u))
+	for i := range u {
+		v[i] = &u[i]
+	}
+
+	users := v
+	testUserName(users)
+}
+
+func testUserName(users []Userer) {
+	for _, u := range users {
+		fmt.Println(u.GetName())
+	}
+}
